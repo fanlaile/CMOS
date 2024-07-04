fb43c4527b033a9f16e065b017f2b642aa116891
fanziqi <fanziqi@piocreat3d.com>
2024-07-04 16:16:00 +0800

    2024/7/4

---
+++ CMOS_V3.2/Core/Inc/adc.h
@@ -0,0 +1,63 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    adc.h
+  * @brief   This file contains all the function prototypes for
+  *          the adc.c file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __ADC_H__
+#define __ADC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern ADC_HandleTypeDef hadc;
+
+/* USER CODE BEGIN Private defines */
+typedef struct  {
+	uint8_t SP_STATUS;
+	uint8_t LSD_START;
+	uint8_t LSD_INT;
+	uint8_t CMOS_START;
+	uint16_t LSD_OFFSET;
+	uint16_t LSD_ADC[LSD_SIZE/2];
+	float LSD_VALUE[5];
+}LSD_Struct;
+
+
+/* USER CODE END Private defines */
+
+void MX_ADC_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+uint32_t adc_get_result(void);
+void ADC_dispose (void);
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ADC_H__ */
+
---
+++ CMOS_V3.2/MDK-ARM/CMOS/startup_stm32f030x6.d
@@ -0,0 +1,1 @@
+cmos\startup_stm32f030x6.o: startup_stm32f030x6.s
